0bf436d7fdd72ff9989a6965ef7ad95f128b8cb6
João Vicente Gaidzinski <gaidzinski2110@gmail.com>
2024-10-27 20:45:59 -0300

    Adiciona baralho e cartas em cena para testar funcionamento

--- Assets/Scripts/Baralho.cs
+++ Assets/Scripts/Baralho.cs
@@ -2,14 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TipoBaralhoEnum
+{
+    ABERTO,
+    FECHADO
+}
+
 public class Baralho : MonoBehaviour
 {
+    [SerializeField]
     private List<Carta> cartas;
+    [SerializeField]
+    private TipoBaralhoEnum tipoBaralho = TipoBaralhoEnum.FECHADO;
+
+    private void Start()
+    {
+        embaralhar();
+    }
 
     public void embaralhar()
     {
         int tamanhoBaralho = cartas.Count;
-        List<Carta> embaralhado = new List<Carta>(tamanhoBaralho);
+        List<Carta> embaralhado = new List<Carta>();
+        
+        for(int i=0; i<tamanhoBaralho; i++)
+        {
+            embaralhado.Add(null);
+        }
+
         for(int i = 0; i < tamanhoBaralho; i++)
         {
             int newIndex = Random.Range(0, tamanhoBaralho);
@@ -27,10 +47,19 @@ public class Baralho : MonoBehaviour
         cartas.Add(carta);
     }
 
-    public Carta retirarCarta()
+    //mudar Retorno para Carta
+    public void retirarCarta()
     {
         Carta carta = cartas[0];
         cartas.Remove(carta);
-        return carta;
+        onRetirarCarta(carta);
+        //return carta;
     }
+
+    private void onRetirarCarta(Carta carta)
+    {
+        Transform cartaTransform = carta.transform;
+        cartaTransform.position = transform.position + new Vector3(4f, 0, 0);
+    }
+
 }
--- Assets/Scripts/Sistema De Cartas/CartaDeTesouro.cs
+++ Assets/Scripts/Sistema De Cartas/CartaDeTesouro.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CartaDeTesouro : MonoBehaviour
+public abstract class CartaDeTesouro : Carta
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void executarAcao()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        throw new System.NotImplementedException();
     }
 }
